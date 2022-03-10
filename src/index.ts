dd19ec762182c7a2bfdb13a5c9bf12ec12878a1e
Luciano SD <94127650+LsDri92@users.noreply.github.com>
2022-03-09 23:36:10 -0300

    agregueezio

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Application, Sprite } from 'pixi.js'
+import { Application, Loader, Sprite } from 'pixi.js'
 
 const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
@@ -9,11 +9,23 @@ const app = new Application({
 	height: 480
 });
 
-const clampy: Sprite = Sprite.from("./clampy.png");
+Loader.shared.add({ url: "./ezio.png", name: "ezioauditore" });
+Loader.shared.add({ url: "./clampy.png", name: "Clampy" });
 
-clampy.anchor.set(0.5);
+Loader.shared.onComplete.add(() => {
 
-clampy.x = app.screen.width / 2;
-clampy.y = app.screen.height / 2;
+	const clampy: Sprite = Sprite.from("ezioauditore");
 
-app.stage.addChild(clampy);
+	console.log("Hola Mundo", clampy.width, clampy.height );
+
+	// clampy.anchor.set(0);
+
+	clampy.x = 0;
+	clampy.y = 0;
+
+	app.stage.addChild(clampy);
+
+});
+
+Loader.shared.load();
+
