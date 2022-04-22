06326c5f8f348658aacee47ae690da9f21d3cbb0
Luciano SD <94127650+LsDri92@users.noreply.github.com>
2022-04-21 23:33:19 -0300

    Cat Sprites

--- src/Scenes/TickerScene.ts
+++ src/Scenes/TickerScene.ts
@@ -8,73 +8,144 @@ import { Keyboard } from "../utils/Keyboard";
 
 export class TickerScene extends Container implements IUpdateable {
 
-    private jumpingCatFlash: AnimatedSprite; 
+    private jumpingCatFlash: AnimatedSprite;
     private physCat: PhysicsContainer;
+    private runningCatFlash: AnimatedSprite;
+    private walkingCatFlash: AnimatedSprite;
+    private standingCatFlash: AnimatedSprite;
 
 
-  constructor()
-  {
-      super();
+    constructor() {
+        super();
 
-      //Animated Sprite
-      this.jumpingCatFlash = new AnimatedSprite(
-        [
-            Texture.from("catjump1"),
-            Texture.from("catjump2"),
-            Texture.from("catjump3"),
-            Texture.from("catjump4"),
-            Texture.from("catjump5"),
-            Texture.from("catjump6"),
-            Texture.from("catjump7")
+        //animated sprite stand
 
-        ],
-        false
+        this.standingCatFlash = new AnimatedSprite(
 
-    );
-    this.jumpingCatFlash.play();
-    this.jumpingCatFlash.anchor.set(1.2);
-    this.jumpingCatFlash.animationSpeed = 0.2;
-    this.jumpingCatFlash.scale.set(3);
-    this.jumpingCatFlash.position.x = 40;
-    this.jumpingCatFlash.position.y = 33;
+            [
+                Texture.from("standcat1"),
+                Texture.from("standcat2"),
+                Texture.from("standcat3"),
+                Texture.from("standcat4"),
+            ],
+            true
+        );
+
+        this.standingCatFlash.play();
+        this.standingCatFlash.animationSpeed = 0.2;
+        this.standingCatFlash.scale.set(3);
+        this.standingCatFlash.position.x = 500;
+        this.standingCatFlash.position.y = 450;
+
+        //animated sprite walk
+
+        this.walkingCatFlash = new AnimatedSprite(
+            [
+                Texture.from("walkcat1"),
+                Texture.from("walkcat2"),
+                Texture.from("walkcat3"),
+                Texture.from("walkcat4"),
+                Texture.from("walkcat5"),
+                Texture.from("walkcat6"),
+                Texture.from("walkcat7"),
+                Texture.from("walkcat8"),
+            ],
+            true
+        );
+
+        this.walkingCatFlash.play();
+        this.walkingCatFlash.anchor.set(0.5);
+        this.walkingCatFlash.animationSpeed = 0.2;
+        this.walkingCatFlash.scale.set(3);
+        this.walkingCatFlash.position.x = 400;
+        this.walkingCatFlash.position.y = 350;
+
+        //animated sprite run
+
+        this.runningCatFlash = new AnimatedSprite(
+            [
+                Texture.from("runcat1"),
+                Texture.from("runcat2"),
+                Texture.from("runcat3"),
+                Texture.from("runcat4"),
+                Texture.from("runcat5"),
+                Texture.from("runcat6"),
+                Texture.from("runcat7"),
+                Texture.from("runcat8"),
+            ],
+            true
+
+        );
+
+        this.runningCatFlash.play();
+        this.runningCatFlash.animationSpeed = 0.2;
+        this.runningCatFlash.scale.set(3);
+        this.runningCatFlash.position.x = 300;
+        this.runningCatFlash.position.y = 250;
 
 
-    
 
-    this.physCat = new PhysicsContainer();
-    this.physCat.speed.x = 150;
-    this.physCat.speed.y = 0;
-    this.physCat.acceleration.y = 250;
-    this.addChild(this.physCat);
+        //Animated Sprite jump
+        this.jumpingCatFlash = new AnimatedSprite(
+            [
+                Texture.from("catjump1"),
+                Texture.from("catjump2"),
+                Texture.from("catjump3"),
+                Texture.from("catjump4"),
+                Texture.from("catjump5"),
+                Texture.from("catjump6"),
+                Texture.from("catjump7")
 
-    const auxZero = new Graphics();
-    auxZero.beginFill(0xFF00FF);
-    auxZero.drawCircle(0,0,10);
-    auxZero.endFill();
+            ],
+            false
 
+        );
+        this.jumpingCatFlash.play();
+        this.jumpingCatFlash.anchor.set(1.2);
+        this.jumpingCatFlash.animationSpeed = 0.2;
+        this.jumpingCatFlash.scale.set(3);
+        this.jumpingCatFlash.position.x = 45;
+        this.jumpingCatFlash.position.y = 30;
 
-    this.physCat.addChild(this.jumpingCatFlash);
-    this.physCat.addChild(auxZero);
+       
+
+        this.physCat = new PhysicsContainer();
+        this.physCat.speed.x = 300;
+        this.physCat.speed.y = 100;
+        this.physCat.acceleration.y = 10;
+        this.addChild(this.physCat);
+
+        const auxZero = new Graphics();
+        auxZero.beginFill(0xFF00FF);
+        auxZero.drawCircle(0, 0, 10);
+        auxZero.endFill();
+
+        this.addChild(this.runningCatFlash);
+        this.addChild(this.standingCatFlash);
+        this.addChild(this.walkingCatFlash);
+
+        this.physCat.addChild(this.jumpingCatFlash);
+        this.physCat.addChild(auxZero);
+        
     }
     update(deltaTime: number, deltaFrame: number): void {
         this.jumpingCatFlash.update(deltaFrame); //update animation
 
         //craft deltatime in seconds
-        const dt = deltaTime /1000
+        const dt = deltaTime / 1000
 
         // update physics
 
         this.physCat.update(dt);
 
         //limit horizontal
-        if (this.physCat.x  > WIDTH)
-        {
+        if (this.physCat.x > WIDTH) {
             //limit rigth
             this.physCat.x = WIDTH;
-            this.physCat.speed.x = Math.abs(this.physCat.speed.x)*-1;
+            this.physCat.speed.x = Math.abs(this.physCat.speed.x) * -1;
             this.physCat.scale.x = -1;
         }
-       // else if (this.physCat.x < 0)
+         else if (this.physCat.x < 0)
         {
             //limit left
             this.physCat.x = 0;
@@ -83,29 +154,31 @@ export class TickerScene extends Container implements IUpdateable {
         }
 
         //limit vertical
-        if (this.physCat.y > HEIGHT)
-        {
+        if (this.physCat.y > HEIGHT) {
             this.physCat.y = HEIGHT;
             this.physCat.speed.y = -500 * Math.random();
 
         }
 
-      
 
-        
-        if (Keyboard.state.get("KeyD")){
-            this.jumpingCatFlash.x++;
+
+        if (Keyboard.state.get("KeyD")) {
+            this.walkingCatFlash.x++;
+            this.walkingCatFlash.scale.x= +3;
+        } 
+
+
+        if (Keyboard.state.get("KeyA")) {
+            this.walkingCatFlash.x--;
+            this.walkingCatFlash.scale.x = -3;
         }
-        if (Keyboard.state.get("KeyA")){
-            this.jumpingCatFlash.x--;
+        if (Keyboard.state.get("KeyS")) {
+            this.runningCatFlash.y++;
         }
-        if (Keyboard.state.get("KeyS")){
-            this.jumpingCatFlash.y++;
+        if (Keyboard.state.get("KeyW")) {
+            this.runningCatFlash.y--;
         }
-        if (Keyboard.state.get("KeyW")){
-            this.jumpingCatFlash.y--;
-        }
-        
+
 
     }
 }
--- src/assets.ts
+++ src/assets.ts
@@ -1,4 +1,6 @@
 export const assets = [
+
+    //catjump
     {url: "./catjump/catjump1.png", name: "catjump1"},
     {url: "./catjump/catjump2.png", name: "catjump2"},
     {url: "./catjump/catjump3.png", name: "catjump3"},
@@ -6,6 +8,8 @@ export const assets = [
     {url: "./catjump/catjump5.png", name: "catjump5"},
     {url: "./catjump/catjump6.png", name: "catjump6"},
     {url: "./catjump/catjump7.png", name: "catjump7"},
+    
+    //lvlcomplete
     {url: "./UIlvlcomplete/metalPanel_blueCorner.png", name: "panel"},
     {url: "./UIlvlcomplete/UI-92.png", name: "completebar"},
     {url: "./UIlvlcomplete/UI-98.png", name: "greenbutton"},
@@ -15,15 +19,46 @@ export const assets = [
     {url: "./UIlvlcomplete/lvlcompleteCat2.png", name: "lvlcompletecat2"},
     {url: "./UIlvlcomplete/lvlcompleteCat3.png", name: "lvlcompletecat3"},
     {url: "./UIlvlcomplete/lvlcompleteCat4.png", name: "lvlcompletecat4"},
+    
+    //bground
     {url: "./back.png", name: "backgroundimg"},
+
+    //platform
     {url: "./platform-long.png", name: "longplatform"},
+
+    //UIpause
     {url: "./UIpause/UI-8.png", name: "cartelpause"},
     {url: "./UIpause/glassPanel_corners.png", name: "pausewindow"},
     {url: "./UIpause/musicOn.png", name: "musicOn"},
     {url: "./UIpause/forward.png", name: "play"},
     {url: "./UIpause/forwardon.png", name: "playon"},
-    {url: "./UIpause/musicOff.png", name: "musicOff"}
+    {url: "./UIpause/musicOff.png", name: "musicOff"},
+
+    //runcat
+    {url: "./runcat/runcat1.png", name: "runcat1"},
+    {url: "./runcat/runcat2.png", name: "runcat2"},
+    {url: "./runcat/runcat3.png", name: "runcat3"},
+    {url: "./runcat/runcat4.png", name: "runcat4"},
+    {url: "./runcat/runcat5.png", name: "runcat5"},
+    {url: "./runcat/runcat6.png", name: "runcat6"},
+    {url: "./runcat/runcat7.png", name: "runcat7"},
+    {url: "./runcat/runcat8.png", name: "runcat8"},
     
+    //standcat
+    {url: "./standcat/standcat1.png", name: "standcat1"},
+    {url: "./standcat/standcat2.png", name: "standcat2"},
+    {url: "./standcat/standcat3.png", name: "standcat3"},
+    {url: "./standcat/standcat4.png", name: "standcat4"},
+
+    //walkcat
+    {url: "./walkcat/walkcat1.png", name: "walkcat1"},
+    {url: "./walkcat/walkcat2.png", name: "walkcat2"},
+    {url: "./walkcat/walkcat3.png", name: "walkcat3"},
+    {url: "./walkcat/walkcat4.png", name: "walkcat4"},
+    {url: "./walkcat/walkcat5.png", name: "walkcat5"},
+    {url: "./walkcat/walkcat6.png", name: "walkcat6"},
+    {url: "./walkcat/walkcat7.png", name: "walkcat7"},
+    {url: "./walkcat/walkcat8.png", name: "walkcat8"},
 
 
 ]
